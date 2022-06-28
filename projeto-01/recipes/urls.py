d89d37fa7dcdf8b258ef5c31906a43d24ae66dd9
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2022-06-27 22:46:58 -0300

    conceito de models

--- projeto-01/recipes/urls.py
+++ projeto-01/recipes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe_show),
+    path('', views.home, name='recipes-home'),
+    path('recipes/<int:id>/', views.recipe_show, name='recipes-recipe'),
 
 ]
--- projeto-01/recipes/views.py
+++ projeto-01/recipes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(req):
-    return render(req, 'recipes/pages/home.html')
+    return render(req, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(9)]
+    })
 
 
 def recipe_show(req, id):
-    return render(req, 'recipes/pages/recipe_show.html')
+    return render(req, 'recipes/pages/recipe_show.html', context={
+        'recipe': make_recipe(),
+        'is_detail': True
+    })
